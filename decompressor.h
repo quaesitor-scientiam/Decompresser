275568b77406b39d188d42411bd726fa61950c30
Richard Wheeler <18647491+PythonWillRule@users.noreply.github.com>
2025-11-19 08:40:04 -0500

    cleanup

--- decompressor.h
+++ decompressor.h
@@ -9,15 +9,6 @@ typedef struct {
 	uint8_t output[];
 } Result;
 
-const char COMPRESSIOM_TYPES[] = {
-	'0', // No compression
-	'1', // RLE Konami
-	'2', // RLE Bakutoshu
-	'3', // LZSS
-	'4', // Huffman
-	'5'  // Custom / Other
-};
-
 Result* KOMAMI_decompressor(int offset, LIBROM* rom);
 uint8_t read_byte(LIBROM* rom);
 
